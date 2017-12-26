fbb37dc4aadfb78d0eae70b655b4a46b33cdf9bd
zhaohejing2017 <zhaohejing@zhj125.cn>
2017-12-26 14:45:35 +0800

    wwww

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,23 +1,94 @@
 <template>
   <div>
-    <grid :cols="3">
-      <grid-item v-for="i in 9" :key="i">
-        <span class="grid-center">{{i}}</span>
-      </grid-item>
-    </grid>
+     <swiper :list="demo03_list"
+      auto
+      style="margin:0 auto;"
+       height="180px"
+        dots-class="custom-bottom"
+         dots-position="center"></swiper>
+     <panel header="商品列表" :footer="footer" :list="list" type="5"></panel>
+
   </div>
 </template>
 
 <script>
-import { Grid, GridItem } from "vux";
+import { Grid, GridItem, Swiper, Panel } from "vux";
+const imgList = [
+  "http://placeholder.qiniudn.com/800x300/FF3B3B/ffffff",
+  "http://placeholder.qiniudn.com/800x300/FFEF7D/ffffff",
+  "http://placeholder.qiniudn.com/800x300/8AEEB1/ffffff"
+];
+
+const demoList = imgList.map(one => ({
+  url: "javascript:",
+  img: one
+}));
 export default {
   components: {
     Grid,
-    GridItem
+    GridItem,
+    Swiper,
+    Panel
   },
   data() {
     return {
-      msg: "Hello World!"
+      msg: "Hello World!",
+      demo03_list: demoList,
+      list: [
+        {
+          src: "http://somedomain.somdomain/x.jpg",
+          fallbackSrc: "http://placeholder.qiniudn.com/60x60/3cc51f/ffffff",
+          title: "标题一",
+          desc: "由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。",
+          url: "/component/cell"
+        },
+        {
+          src: "http://somedomain.somdomain/x.jpg",
+          fallbackSrc: "http://placeholder.qiniudn.com/60x60/3cc51f/ffffff",
+          title: "标题一",
+          desc: "由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。",
+          url: "/component/cell"
+        },
+        {
+          src: "http://somedomain.somdomain/x.jpg",
+          fallbackSrc: "http://placeholder.qiniudn.com/60x60/3cc51f/ffffff",
+          title: "标题一",
+          desc: "由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。",
+          url: "/component/cell"
+        },
+        {
+          src: "http://somedomain.somdomain/x.jpg",
+          fallbackSrc: "http://placeholder.qiniudn.com/60x60/3cc51f/ffffff",
+          title: "标题一",
+          desc: "由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。",
+          url: "/component/cell"
+        },
+        {
+          src: "http://somedomain.somdomain/x.jpg",
+          fallbackSrc: "http://placeholder.qiniudn.com/60x60/3cc51f/ffffff",
+          title: "标题一",
+          desc: "由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。",
+          url: "/component/cell"
+        },
+        {
+          src: "http://placeholder.qiniudn.com/60x60/3cc51f/ffffff",
+          title: "标题二",
+          desc: "由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。",
+          url: {
+            path: "/component/radio",
+            replace: false
+          },
+          meta: {
+            source: "来源信息",
+            date: "时间",
+            other: "其他信息"
+          }
+        }
+      ],
+      footer: {
+        title: "显示更多",
+        url: "http://vux.li"
+      }
     };
   }
 };
--- src/views/my/my.vue
+++ src/views/my/my.vue
@@ -1,31 +1,47 @@
 <template>
-  <div>
-    我的
+  <div style="">
+    <blur :blur-amount=40 :url="url">
+      <p class="center"><img :src="url"></p>
+    </blur>
+    <flexbox :gutter="0">
+      <flexbox-item v-for="(img, index) in images" :key="index"><img :src="img" style="width:100%" @click="url = img"/></flexbox-item>
+    </flexbox>
   </div>
 </template>
 
 <script>
-import { Group, Cell } from "vux";
+import { Flexbox, FlexboxItem, Blur } from "vux";
 
 export default {
   components: {
-    Group,
-    Cell
+    Blur,
+    Flexbox,
+    FlexboxItem
   },
   data() {
     return {
-      msg: "Hello World!"
+      images: [
+        "https://o3e85j0cv.qnssl.com/tulips-1083572__340.jpg",
+        "https://o3e85j0cv.qnssl.com/waterway-107810__340.jpg",
+        "https://o3e85j0cv.qnssl.com/hot-chocolate-1068703__340.jpg"
+      ],
+      url: "https://o3e85j0cv.qnssl.com/tulips-1083572__340.jpg"
     };
   }
 };
 </script>
 
-<style>
-.vux-demo {
+<style scoped>
+.center {
   text-align: center;
+  padding-top: 20px;
+  color: #fff;
+  font-size: 18px;
 }
-.logo {
+.center img {
   width: 100px;
   height: 100px;
+  border-radius: 50%;
+  border: 4px solid #ececec;
 }
 </style>
